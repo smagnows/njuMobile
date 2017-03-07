0e85a7538d3fb3939d0260cf5b297db5126a80d0
smagnows <s.magnowski@outlook.com>
2017-03-07 18:50:53 +0100

    Pierwsza wersja

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	printf("*** couty i ciny xd\n");
+	puts("Fpisz sfoje imje");
+	char cosisi[21];
+	scanf("%20c", cosisi);
+	printf("Siema, %s", cosisi);
+return 0;
+}
